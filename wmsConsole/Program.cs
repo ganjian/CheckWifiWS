2db364cfaac03414bde1fe4d8d8e3a78c7a1efc1
ganjian <ganjian@outlook.com>
2015-09-01 22:13:34 +0800

    添加对网络的实时监控

---
+++ wmsConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wmsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkChange.NetworkAvailabilityChanged += NetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged += NetworkAddressChanged;
+            Console.ReadKey();
+        }
+        private static void NetworkAddressChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Current IP Addresses:");
+            Debug.Write("Current IP Addresses:");
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation addr in ni.GetIPProperties().UnicastAddresses)
+                {
+                    Console.WriteLine("    - {0} (lease expires {1})", addr.Address, DateTime.Now + new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime));
+                    Debug.Write(string.Format("    - {0} (lease expires {1})", addr.Address, DateTime.Now + new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime)));
+                }
+            }
+        }
+
+        private static void NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            if (e.IsAvailable)
+            {
+                Console.WriteLine("Network Available");
+                Debug.Write("Network Available");
+            }
+            else
+            {
+                Debug.Write("Network Unavailable");
+                Console.WriteLine("Network Unavailable");
+            }
+        }
+    }
+}
--- wmsWF/Form1.cs
+++ wmsWF/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,8 @@ namespace wmsWF
         public Form1()
         {
             InitializeComponent();
+            NetworkChange.NetworkAvailabilityChanged += NetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged += NetworkAddressChanged;
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -23,7 +26,7 @@ namespace wmsWF
             {
                 this.ShowInTaskbar = false;//不显示在系统任务栏
                 notifyIcon1.Visible = true;//托盘显示可见
-              
+
             }
         }
 
@@ -38,5 +41,99 @@ namespace wmsWF
         }
 
 
+
+        private static void NetworkAddressChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Current IP Addresses:");
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation addr in ni.GetIPProperties().UnicastAddresses)
+                {
+                    MessageBox.Show(string.Format(("    - {0} (lease expires {1})"), addr.Address, DateTime.Now + new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime)));
+                }
+            }
+        }
+
+        private static void NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            if (e.IsAvailable)
+            {
+                MessageBox.Show("Network Available");
+            }
+            else
+            {
+                MessageBox.Show("Network Unavailable");
+            }
+        }
+
+        private const int INTERNET_CONNECTION_MODEM = 1;
+        private const int INTERNET_CONNECTION_LAN = 2;
+
+        [System.Runtime.InteropServices.DllImport("winInet.dll")]
+        private static extern bool InternetGetConnectedState(ref int dwFlag, int dwReserved);
+
+        /// <summary>
+        /// 判断本地的连接状态
+        /// </summary>
+        /// <returns></returns>
+        private static bool LocalConnectionStatus()
+        {
+            System.Int32 dwFlag = new Int32();
+            if (!InternetGetConnectedState(ref dwFlag, 0))
+            {
+                Console.WriteLine("LocalConnectionStatus--未连网!");
+                return false;
+            }
+            else
+            {
+                if ((dwFlag & INTERNET_CONNECTION_MODEM) != 0)
+                {
+                    Console.WriteLine("LocalConnectionStatus--采用调制解调器上网。");
+                    return true;
+                }
+                else if ((dwFlag & INTERNET_CONNECTION_LAN) != 0)
+                {
+                    Console.WriteLine("LocalConnectionStatus--采用网卡上网。");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// Ping命令检测网络是否畅通
+        /// </summary>
+        /// <param name="urls">URL数据</param>
+        /// <param name="errorCount">ping时连接失败个数</param>
+        /// <returns></returns>
+        public static bool MyPing(string[] urls, out int errorCount)
+        {
+            bool isconn = true;
+            Ping ping = new Ping();
+            errorCount = 0;
+            try
+            {
+                PingReply pr;
+                for (int i = 0; i < urls.Length; i++)
+                {
+                    pr = ping.Send(urls[i]);
+                    if (pr.Status != IPStatus.Success)
+                    {
+                        isconn = false;
+                        errorCount++;
+                    }
+                    Console.WriteLine("Ping " + urls[i] + "    " + pr.Status.ToString());
+                }
+            }
+            catch
+            {
+                isconn = false;
+                errorCount = urls.Length;
+            }
+            //if (errorCount > 0 && errorCount < 3)
+            //  isconn = true;
+            return isconn;
+        }
     }
 }
